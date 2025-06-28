24f45a5996f96d2c3ee90f03b53ec7f7c508e3af
Eugene <et12sova@gmail.com>
2025-06-28 15:27:50 +0700

    added nosuchelementexeption

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
-    
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+
